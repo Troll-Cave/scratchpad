7923dd275eebacd1eed1863dc2a9d761e6bf811a
Tom <whynotavailable@protonmail.com>
2021-11-17 09:47:37 -0500

    Added angle

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,6 +8,7 @@ public class Movement : MonoBehaviour
     float yMove = 0;
 
     private PlayerController playerController;
+    private Quaternion rotation = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +21,25 @@ public class Movement : MonoBehaviour
     {
         xMove = Input.GetAxisRaw("Horizontal") * 2.5f;
         yMove = Input.GetAxisRaw("Vertical") * 2.5f;
+
+        var mousePosition = Input.mousePosition;
+
+        var newPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        var swordPosition = transform.position;
+
+        var diff = newPosition - swordPosition;
+
+        var angle = Mathf.Atan2(diff.x, diff.y) * Mathf.Rad2Deg;
+
+        //transform.forward = diff;
+        
+        rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
     }
 
     private void FixedUpdate()
     {
         playerController.Move(xMove * Time.deltaTime, yMove * Time.deltaTime);
+        //transform.Rotate(new Vector3(0, 0, 50));
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, .2f);
     }
 }

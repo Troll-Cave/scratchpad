54820158a1e45e9d837a4f00318e1a8b211fd5a0
Tom <whynotavailable@protonmail.com>
2021-11-17 20:33:32 -0500

    update to UI

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -5,15 +5,33 @@ using UnityEngine.UIElements;
 
 public class UI : MonoBehaviour
 {
+    private ProgressBar healthBar;
+    private ProgressBar manaBar;
+
     // Start is called before the first frame update
     void Start()
     {
+        healthBar = GetComponent<UIDocument>().rootVisualElement.Query<ProgressBar>("Health").First();
+        manaBar = GetComponent<UIDocument>().rootVisualElement.Query<ProgressBar>("Mana").First();
+
+        healthBar.value = 100;
+        manaBar.value = 100;
+
+        healthBar.title = $"{healthBar.value}/100";
+        manaBar.title = $"{manaBar.value}/100";
+
         GetComponent<UIDocument>().rootVisualElement.Query<Button>("Clicker").First().clicked += () => UpdateHealth();
     }
 
+    // The really nice thing is that this doesn't run inside the update loop
     void UpdateHealth()
     {
-        GetComponent<UIDocument>().rootVisualElement.Query<ProgressBar>().First().value += 5;
+        healthBar.value -= Random.Range(5, 10);
+
+        healthBar.title = $"{healthBar.value}/100";
+        manaBar.title = $"{manaBar.value}/100";
+
+        manaBar.value -= Random.Range(5, 10);
     }
 
     // Update is called once per frame
